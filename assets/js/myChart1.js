b54826d9d31d3463ebb36a5317920f2667f12470
Pablo Vera Terán <pabloverat@MacBook-Air-de-Pablo.local>
2020-12-13 11:04:49 -0600

    added chart to autogestión section

--- assets/js/myChart1.js
+++ assets/js/myChart1.js
@@ -1,6 +1,6 @@
 var oilCanvas = document.getElementById("oilChart");
 
-Chart.defaults.global.defaultFontFamily = "Lato";
+Chart.defaults.global.defaultFontFamily = "Helvetica";
 Chart.defaults.global.defaultFontSize = 18;
 
 var oilData = {
@@ -8,16 +8,11 @@ var oilData = {
         "Adjudicaciones Directas",
         "Invitación Restringida",
         "Licitación Pública",
-        "Otros"
+        "Otros (Contratos entre entes públicos y contrataciones con entes externos)"
     ],
     datasets: [{
         data: [78.16, 8.18, 13.21, 0.45],
-        backgroundColor: [
-            "#FF6384",
-            "#63FF84",
-            "#84FF63",
-            "#6384FF"
-        ]
+        backgroundColor: ['#011d27', '#028090', '#f8f8f8', '#8acdd0']
     }]
 };
 
